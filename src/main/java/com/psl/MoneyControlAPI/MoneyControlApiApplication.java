da45536bb1455b6bb21ca424e94c8d61442874f5
Krishna Rawat <krishna_rawat@persistent.com>
2023-03-17 18:24:51 +0530

    Basic Template

---
+++ src/main/java/com/psl/MoneyControlAPI/MoneyControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.psl.MoneyControlAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyControlApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/psl/MoneyControlAPI/service/MoneyControlService.java
@@ -0,0 +1,27 @@
+package com.psl.MoneyControlAPI.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MoneyControlService {
+
+    public void getStocks(){
+        final String url = "https://www.moneycontrol.com/mutual-funds/performance-tracker/returns/equity---etfindex.html";
+        try{
+            final Document document = Jsoup.connect(url).get();
+//			System.out.println(document.outerHtml());
+            for(Element row: document.select("table.mctable1 tr")) {
+
+                final String schemeName =  row.select("td:nth-of-type(1)").text();
+                final String auM =  row.select("td:nth-of-type(5)").text();
+                final String week =  row.select("td:nth-of-type(6)").text();
+                System.out.println(schemeName+" --> "+auM+" "+week);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
