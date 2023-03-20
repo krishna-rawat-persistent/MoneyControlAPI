6db395b4de5a343510ce8ebfabc6067bd86c15d5
Krishna Rawat <krishna_rawat@persistent.com>
2023-03-20 11:28:10 +0530

    model file added

--- src/main/java/com/psl/MoneyControlAPI/controller/StocksFilterController.java
+++ src/main/java/com/psl/MoneyControlAPI/controller/StocksFilterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 @CrossOrigin()
-public class StocksController {
+public class StocksFilterController {
 
     @GetMapping("/")
     public String welcomeMsg(){
