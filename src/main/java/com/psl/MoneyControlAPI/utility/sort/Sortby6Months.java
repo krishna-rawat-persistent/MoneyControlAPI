ac035d9ea8601fca2df1df317522acd4db6cd6b8
Krishna Rawat <krishna_rawat@persistent.com>
2023-03-28 10:52:37 +0530

    sorting logic modified

--- src/main/java/com/psl/MoneyControlAPI/utility/sort/Sortby6Months.java
+++ src/main/java/com/psl/MoneyControlAPI/utility/sort/Sortby6Months.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class Sortby6Months implements Comparator<StockFilter> {
     @Override
     public int compare(StockFilter o1, StockFilter o2) {
-        if (o1.getSixMonth() < o2.getSixMonth()) return 1;
+        if (o1.getSixMonth() < o2.getSixMonth() || (o1.getSixMonth() == 0)) return 1;
         if (o1.getSixMonth() > o2.getSixMonth()) return -1;
         return 0;
     }
