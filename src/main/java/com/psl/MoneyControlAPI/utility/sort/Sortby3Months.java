9a937b57a5e6b6c44edf70aa01f2d062ba67132e
Krishna Rawat <krishna_rawat@persistent.com>
2023-03-30 15:51:25 +0530

    Sorting Logic Changed

--- src/main/java/com/psl/MoneyControlAPI/utility/sort/Sortby3Months.java
+++ src/main/java/com/psl/MoneyControlAPI/utility/sort/Sortby3Months.java
@@ -7,8 +7,6 @@ import java.util.Comparator;
 public class Sortby3Months implements Comparator<StockFilter> {
     @Override
     public int compare(StockFilter o1, StockFilter o2) {
-        if (o1.getThreeMonth() < o2.getThreeMonth()) return 1;
-        if (o1.getThreeMonth() > o2.getThreeMonth() && o1.getThreeMonth() !=0) return -1;
-        return 0;
+        return ComparisonLogic.logic(o1.getThreeMonth(),o2.getThreeMonth());
     }
 }
--- src/main/java/com/psl/MoneyControlAPI/utility/sort/Sortby5Years.java
+++ src/main/java/com/psl/MoneyControlAPI/utility/sort/Sortby5Years.java
@@ -7,8 +7,6 @@ import java.util.Comparator;
 public class Sortby5Years implements Comparator<StockFilter> {
     @Override
     public int compare(StockFilter o1, StockFilter o2) {
-        if (o1.getFiveYear() < o2.getFiveYear()) return 1;
-        if (o1.getFiveYear() > o2.getFiveYear() && o1.getFiveYear() !=0) return -1;
-        return 0;
+        return ComparisonLogic.logic(o1.getFiveYear(),o2.getFiveYear());
     }
 }
