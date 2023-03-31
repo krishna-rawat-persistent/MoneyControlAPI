acb55337733b2fe26c8aa3f03f8c017525553fc2
Krishna Rawat <krishna_rawat@persistent.com>
2023-03-31 12:23:08 +0530

    Comparison logic added

--- src/main/java/com/psl/MoneyControlAPI/utility/sort/ComparisonLogic.java
+++ src/main/java/com/psl/MoneyControlAPI/utility/sort/ComparisonLogic.java
@@ -5,6 +5,7 @@ import com.psl.MoneyControlAPI.model.StockFilter;
 public class ComparisonLogic {
 
     public static int logic(Double n1, Double n2){
+
         Double val1 = n1 == 0? -Double.MAX_VALUE : n1;
         Double val2 = n2 == 0? -Double.MAX_VALUE : n2;
         if (val1 < val2) return 1;
