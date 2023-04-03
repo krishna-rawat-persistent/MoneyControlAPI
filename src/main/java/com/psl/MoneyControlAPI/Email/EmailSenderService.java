f2250a354c7b79d1645b94657838207b5f2810d2
Nancy Gupta <nancy_gupta@persistent.com>
2023-04-03 10:45:29 +0530

    email logic added

---
+++ src/main/java/com/psl/MoneyControlAPI/Email/EmailSenderService.java
@@ -0,0 +1,37 @@
+package com.psl.MoneyControlAPI.Email;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+@Service
+public class EmailSenderService {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public void sendEmail(String subject, String body) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("nancygupta7799@gmail.com");
+		message.setTo(sendEmailIds());
+		message.setText(body);
+		message.setSubject(subject);
+		
+		
+		mailSender.send(message);
+		
+		System.out.println("Mail Sent Successfully....");
+	}
+
+	
+	public String[] sendEmailIds() {
+		String[] emailIds = new String[2];
+		emailIds[0] = "nancygupta426@gmail.com";
+		emailIds[1] = "kavitag767@gmail.com";
+		return emailIds;
+		}
+	
+	
+}
--- src/main/java/com/psl/MoneyControlAPI/controller/StocksFilterController.java
+++ src/main/java/com/psl/MoneyControlAPI/controller/StocksFilterController.java
@@ -1,7 +1,10 @@
 package com.psl.MoneyControlAPI.controller;
 
+import com.psl.MoneyControlAPI.Email.EmailSenderService;
 import com.psl.MoneyControlAPI.model.StockFilter;
 import com.psl.MoneyControlAPI.service.StockFilterService;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,9 @@ public class StocksFilterController {
 
     @Autowired
     StockFilterService service;
+    
+    @Autowired
+   	private EmailSenderService senderService;
 
     @GetMapping("/")
     public String welcomeMsg(){
@@ -24,4 +30,12 @@ public class StocksFilterController {
     public List<StockFilter> getStocks(@PathVariable("url") String url, @PathVariable("duration") String[] duration){
         return service.getStocks(url,duration);
     }
+  
+
+	@GetMapping("/emailSend")
+	public String sendMail() {
+		senderService.sendEmail("This is subject", "This is body");
+		return "Mail Sent successfully...";
+
+	}
 }
